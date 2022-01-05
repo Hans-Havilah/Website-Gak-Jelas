fef4df90dd9f777b5390f1f4b723cff64a7ab9be
Hans Havilah Fadhlan <91402673+Hans-Havilah@users.noreply.github.com>
2022-01-05 13:23:49 +0700

    Add files via upload

---
+++ imk/tari.js
@@ -0,0 +1,30 @@
+var a;
+function show_hide ()
+{
+    if(a==0)
+    {
+            document.getElementById("image").style.display="inline";
+            return a=1;
+            
+    }
+    else
+        {
+            document.getElementById("image").style.display="none";
+            return a=0;
+        }
+}
+
+var a;
+function show_hide1 ()
+{
+    if(a==0)
+    {
+        document.getElementById("image2").style.display="inline";
+        return a=1;
+    }
+    else
+        {
+            document.getElementById("image2").style.display="none";
+            return a=0;
+        }
+}
